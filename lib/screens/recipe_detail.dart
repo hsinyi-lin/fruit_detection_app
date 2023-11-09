7613e18a5bc6424ef10cceb8487848757a42dd80
林欣儀 <59244692+hsinyi-lin@users.noreply.github.com>
2023-11-10 00:31:08 +0800

    refactor: recipe detail, add recipe id

--- lib/screens/recipe_detail.dart
+++ lib/screens/recipe_detail.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
 class RecipeDetailScreen extends StatelessWidget {
+  final int id;
   final String imagePath;
   final String recipeName;
 
-  RecipeDetailScreen({required this.imagePath, required this.recipeName});
+  RecipeDetailScreen({
+    required this.id,
+    required this.imagePath,
+    required this.recipeName,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -12,19 +17,35 @@ class RecipeDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(recipeName),
       ),
-      body: RecipeDetailContent(imagePath: imagePath, recipeName: recipeName),
+      body: RecipeDetailContent(id: id, imagePath: imagePath, recipeName: recipeName),
     );
   }
 }
 
 class RecipeDetailContent extends StatelessWidget {
+  final int id;
   final String imagePath;
   final String recipeName;
 
-  RecipeDetailContent({required this.imagePath, required this.recipeName});
+  RecipeDetailContent({
+    required this.id,
+    required this.imagePath,
+    required this.recipeName,
+  });
+
+  // 假設的數據源，可以替換為實際的數據源
+  final Map<int, String> recipeData = {
+    1: '這是有關 美味食譜1 的詳細資訊。',
+    2: '這是有關 美味食譜2 的詳細資訊。',
+    3: '這是有關 美味食譜3 的詳細資訊。',
+    4: '這是有關 美味食譜4 的詳細資訊。',
+  };
 
   @override
   Widget build(BuildContext context) {
+    // 根據 id 獲取食譜的詳細資訊
+    final String recipeInfo = recipeData[id] ?? '沒有找到相應的食譜資訊';
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
@@ -37,7 +58,7 @@ class RecipeDetailContent extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.all(16.0),
           child: Text(
-            '食譜資訊：這是有關 $recipeName 的詳細資訊。',
+            '食譜資訊：$recipeInfo',
             style: TextStyle(fontSize: 18),
           ),
         ),
