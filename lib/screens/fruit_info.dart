ca7fc2187be79f3a5d3aca44f3a42fc23d3e84f1
林欣儀 <59244692+hsinyi-lin@users.noreply.github.com>
2023-11-09 23:39:29 +0800

    feat: add fruit_info dialog

--- lib/screens/fruit_info.dart
+++ lib/screens/fruit_info.dart
@@ -11,16 +11,19 @@ class FruitInfo extends StatelessWidget {
         'id': 1,
         'icon': Icons.star,
         'name': '蘋果',
+        'description': '蘋果是一種美味且營養豐富的水果。',
       },
       {
         'id': 2,
         'icon': Icons.local_florist,
         'name': '香蕉',
+        'description': '香蕉是一種富含鉀和維生素的水果。',
       },
       {
         'id': 3,
-        'icon': Icons.ac_unit,
+        'icon': Icons.local_florist,
         'name': '葡萄',
+        'description': '葡萄是一種天然的抗氧化劑。',
       },
       // 添加更多水果資訊
     ];
@@ -34,6 +37,7 @@ class FruitInfo extends StatelessWidget {
             id: fruits[index]['id'],
             icon: fruits[index]['icon'],
             name: fruits[index]['name'],
+            description: fruits[index]['description'],
           );
         },
       ),
@@ -45,19 +49,44 @@ class FruitInfoItem extends StatelessWidget {
   final int id;
   final IconData icon;
   final String name;
+  final String description;
 
   const FruitInfoItem({
     Key? key,
     required this.id,
     required this.icon,
     required this.name,
+    required this.description,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       leading: Icon(icon),
-      title: Text(name),
+      title: Text('$name'),
+      onTap: () {
+        _showFruitDetails(context);
+      },
+    );
+  }
+
+  void _showFruitDetails(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('$name (ID: $id)'),
+          content: Text(description),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('關閉'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
